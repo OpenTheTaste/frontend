78709ed6e3ad9ca3efdeaccf91abe1b715263122
joooii <3098k@naver.com>
2026-02-13 13:37:22 +0900

    [OT-28] [FEAT]: InterfactionButton 컴포넌트 size 추가

--- src/components/common/InteractionButton.tsx
+++ src/components/common/InteractionButton.tsx
@@ -4,14 +4,37 @@ interface InteractionButtonProps {
   type: "like" | "bookmark";
   isActive: boolean;
   onAction: () => void;
+  size?: "lg" | "md" | "sm";
 }
 
-export const InteractionButton = ({ type, isActive, onAction }: InteractionButtonProps) => {
+const sizeConfig = {
+  lg: {
+    button: "w-18 h-14", // 72 * 56 px
+    image: { width: 72, height: 56 },
+  },
+  md: {
+    button: "w-14 h-11", // 56 * 44 px
+    image: { width: 56, height: 44 },
+  },
+  sm: {
+    button: "w-12 h-9", // 48 * 36 px
+    image: { width: 48, height: 36 },
+  },
+};
+
+export const InteractionButton = ({
+  type,
+  isActive,
+  onAction,
+  size = "lg",
+}: InteractionButtonProps) => {
+  const config = sizeConfig[size];
+
   return (
     // size : 72 * 56 px, radius : 36px
     <button
       onClick={onAction}
-      className="flex items-center justify-center w-18 h-14 rounded-[2.25rem] shrink-0"
+      className={`${config.button} ${config.image} flex items-center justify-center rounded-[2.25rem] shrink-0 cursor-pointer`}
     >
       <Image
         src={
@@ -24,8 +47,8 @@ export const InteractionButton = ({ type, isActive, onAction }: InteractionButto
               : "/icons/BookmarkDefault.svg"
         }
         alt={`${type} 아이콘`}
-        width={72}
-        height={56}
+        width={config.image.width}
+        height={config.image.height}
         className="object-contain"
       />
     </button>
