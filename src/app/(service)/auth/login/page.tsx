def97a3c30716a6e1169e178312d1fffd42697c5
hyunw-kang <vpffp368@naver.com>
2026-02-14 23:57:07 +0900

    [OT-24] [FEAT]: Login Page 구현

--- src/app/(service)/auth/login/page.tsx
+++ src/app/(service)/auth/login/page.tsx
@@ -1,10 +1,12 @@
 import OnboardHeader from "@/components/Onboard/Header/Header";
+import LoginContent from "@/components/Onboard/login/LoginContent";
 import Footer from "@/components/common/Footer";
 
 export default function Login() { 
     return (
         <div className="bg-background min-h-screen">
             <OnboardHeader />
+            <LoginContent/>
             <Footer/>
         </div>
     )
