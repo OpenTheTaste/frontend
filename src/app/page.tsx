153c09869d8d29cf1bc63b0e8e10ea2f178602fb
hyunw-kang <vpffp368@naver.com>
2026-02-19 11:44:29 +0900

    [OT-39] [FEAT]: 홈 UI 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+import Header from "@/components/common/Header";
+import ContentCarousel from "@/components/home/ContentCarousel";
+import Footer from "@/components/common/Footer";
+
 export default function Home() {
-  <div>홈</div>;
+  return (
+    <div>
+      <Header />
+      <ContentCarousel 
+        title="실시간 인기 차트"
+        itemCount={20}
+        itemWidth={180}
+        itemHeight={240}
+      />
+      <ContentCarousel 
+        title="00님이 좋아하실 콘텐츠"
+        itemCount={20}
+        itemWidth={180}
+        itemHeight={240}
+      />
+      <ContentCarousel
+        title="카테고리 기반 추천 콘텐츠"
+        itemCount={20}
+        itemWidth={180}
+        itemHeight={240}
+      />
+      <Footer />
+    </div>
+  );
 }
