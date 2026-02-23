8bb9e85bed133b2653cc7cab35bc09734b27f4a7
joooii <3098k@naver.com>
2026-02-24 08:27:58 +0900

    [OT-62] [FEAT]: commonBadge, badge 컴포넌트 내 조건 추가

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -1,17 +1,40 @@
 export interface CommonButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  variant?: "primary" | "secondary" | "outline";
 }
 
 export const CommonButton = ({
   children,
   className = "",
+  variant = "primary",
   ...props
 }: CommonButtonProps) => {
+  const baseStyle =
+    "flex items-center justify-center gap-4 rounded-lg cursor-pointer";
+
+  let variantStyle = "";
+
+  switch (variant) {
+    case "primary":
+      variantStyle = "bg-ot-primary-gradient-btn text-ot-text";
+      break;
+
+    case "secondary":
+      variantStyle =
+        "bg-ot-secondary-700 text-ot-text hover:bg-ot-secondary-800";
+      break;
+
+    case "outline":
+      variantStyle =
+        "border border-ot-primary-400 bg-none text-ot-primary-400 hover:bg-ot-primary-100";
+      break;
+
+    default:
+      return;
+  }
+
   return (
-    <button
-      className={`flex items-center justify-center gap-4 rounded-lg bg-ot-primary-gradient-btn cursor-pointer ${className}`}
-      {...props}
-    >
+    <button className={`${baseStyle} ${variantStyle} ${className}`} {...props}>
       {children}
     </button>
   );
