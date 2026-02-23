60037d8941cf9a5fdaedf9676a9c594af9cdd47a
joooii <3098k@naver.com>
2026-02-24 08:35:48 +0900

    [OT-62] [FEAT]: Admin 공통 컴포넌트 구현

--- src/components/admin/common/AdminBadge.tsx
+++ src/components/admin/common/AdminBadge.tsx
@@ -1,23 +1,24 @@
 import { cn } from "@/utils/cn";
 
-type AdminBadgeVariant = "공개" | "비공개" | "관리자" | "사용자" | "활성";
+type AdminBadgeVariant = "공개" | "비공개";
 
+// 뱃지 또 추가할 것 있으면 추가하기
 interface AdminBadgeProps {
   variant: AdminBadgeVariant;
   className?: string;
 }
 
-// const variantStyle: Record<AdminBadgeVariant, string> = {
-//   공개: "bg-[#CCFBF1] text-[#298880]",
-//   비공개: "bg-ot-primary-100 text-[#882929]",
-// };
+const variantStyle: Record<AdminBadgeVariant, string> = {
+  공개: "bg-[#CCFBF1] text-[#298880]",
+  비공개: "bg-ot-primary-100 text-[#882929]",
+};
 
 export default function AdminBadge({ variant, className }: AdminBadgeProps) {
   return (
     <div
       className={cn(
         "inline-flex items-center justify-center text-xs rounded-[1.25rem] px-3 py-0.5 font-medium",
-        // variantStyle[variant],
+        variantStyle[variant],
         className,
       )}
     >
