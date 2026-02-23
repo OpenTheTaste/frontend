3bdbbbcdaacfcecf13c01aac35c3bfc36c1ac634
hyunw-kang <vpffp368@naver.com>
2026-02-24 00:03:25 +0900

    [OT-100] [FEAT]: AdminUser_title 구현

---
+++ src/app/(admin)/admin/user/layout.tsx
@@ -0,0 +1,18 @@
+import { AdminTitle } from "@admin";
+
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <AdminTitle
+        title="사용자 관리"
+        description="일반 사용자 및 에디터를 관리합니다."
+      />
+
+      <div className="px-12 pb-12">{children}</div>
+    </>
+  );
+}
