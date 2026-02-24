030700b5985679c12397bf1e306c51e14e5c5ca9
hyunw-kang <vpffp368@naver.com>
2026-02-24 11:26:51 +0900

    [OT-100] [FIX]: 벡오피스 사용자 관리 UI 수정

--- src/app/(admin)/admin/user/page.tsx
+++ src/app/(admin)/admin/user/page.tsx
@@ -1,13 +1,5 @@
-import { AdminSearch } from "@admin";
-import AdminUserContents from "@/domains/admin/user/components/AdminUserContents";
+import AdminUserSection from "@/domains/admin/user/components/AdminUserSection";
 
 export default function UserPage() {
-  return (
-    <>
-      <AdminSearch
-        placeholder="이름 또는 이메일을 검색하세요."
-      />
-      <AdminUserContents/>
-    </>
-  );
+  return <AdminUserSection />;
 }
