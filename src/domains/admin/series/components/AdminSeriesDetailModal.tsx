9080ad8ea9e1d1efacae1e8b714241820d357da0
hyunw-kang <vpffp368@naver.com>
2026-02-25 11:33:21 +0900

    [OT-108] [FIX]: SeriesDetailModal 뒷 배경 opacity 수정

--- src/domains/admin/series/components/AdminSeriesDetailModal.tsx
+++ src/domains/admin/series/components/AdminSeriesDetailModal.tsx
@@ -17,7 +17,7 @@ export function AdminSeriesDetailModal({ series, onClose }: AdminSeriesDetailMod
 
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/60"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
       onClick={onClose}
     >
       <div
