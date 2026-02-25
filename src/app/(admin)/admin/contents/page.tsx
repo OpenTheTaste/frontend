83946f49aa4b0e4d92823ca1eea69db635332740
joooii <3098k@naver.com>
2026-02-25 13:52:18 +0900

    [OT-106] [FEAT]: Suspence 추가

--- src/app/(admin)/admin/contents/page.tsx
+++ src/app/(admin)/admin/contents/page.tsx
@@ -1,5 +1,12 @@
 import AdminContentsSection from "@/domains/admin/contents/components/AdminContentsSection"; // FIXME:
+import { Suspense } from "react";
 
 export default function AdminContentsPage() {
-  return <AdminContentsSection />;
+  return (
+    <Suspense fallback={null}>
+      {" "}
+      {/*Pre-rendering 지금은 아무것도 없음*/}
+      <AdminContentsSection />
+    </Suspense>
+  );
 }
