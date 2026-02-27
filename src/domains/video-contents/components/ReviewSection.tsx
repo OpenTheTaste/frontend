42bd3a7b5ce3dc85ec07279baef1902f0a4385a9
joooii <3098k@naver.com>
2026-02-27 09:59:42 +0900

    [OT-137] [FIX]: 콘텐츠 댓글 list 데이터 유무에 따른 UI 분기처리

--- src/domains/video-contents/components/ReviewSection.tsx
+++ src/domains/video-contents/components/ReviewSection.tsx
@@ -149,66 +149,76 @@ export default function ReviewSection({
 
         {/* 댓글 목록 - 전체 높이 */}
         <div className="overflow-y-auto flex-1 mt-2">
-          {filteredSpoilerReviews.map((item) => (
-            <div
-              key={item.id}
-              className="flex flex-col p-3 text-ot-text mt-1 border-ot-gray-700 border-b"
-            >
-              {editingReviewId === item.id ? (
-                <>
-                  <textarea
-                    placeholder="댓글을 입력하세요"
-                    value={editingReview}
-                    onChange={(e) => setEditingReview(e.target.value)}
-                    className="w-full text-ot-text placeholder-ot-gray-600 py-2 px-3 border border-ot-gray-800 rounded-lg"
-                  />
-                  <div className="flex items-center justify-end gap-3 mt-3">
-                    <button
-                      onClick={handleCancelEdit}
-                      className="text-sm px-3 py-1 hover:text-ot-gray-600 cursor-pointer"
-                    >
-                      취소
-                    </button>
-                    <CommonButton
-                      onClick={() => handleSaveEditReview(item.id)}
-                      className="px-2 py-1 rounded-sm"
-                    >
-                      <p className="text-sm">저장</p>
-                    </CommonButton>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <p className="flex-nowrap flex">{item.review}</p>
-
-                  <div className="flex items-center">
-                    <div className="flex text-sm pt-1">
-                      <p>
-                        {item.author} ⋅ {item.date}
-                      </p>
-                    </div>
-
-                    {item.myself && (
-                      <div className="flex justify-center items-center ml-auto">
-                        <button
-                          onClick={() => handleEditReview(item.id, item.review)}
-                          className="text-sm flex mr-2 hover:text-ot-gray-600 cursor-pointer"
-                        >
-                          수정
-                        </button>
-                        <button
-                          onClick={() => handleDeleteReview(item.id)}
-                          className="text-sm flex hover:text-ot-gray-600 cursor-pointer"
-                        >
-                          삭제
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                </>
-              )}
+          {filteredSpoilerReviews.length === 0 ? (
+            <div className="flex items-center justify-center h-full">
+              <p className="text-ot-gray-600">작성된 댓글이 없습니다.</p>
             </div>
-          ))}
+          ) : (
+            <div>
+              {filteredSpoilerReviews.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex flex-col p-3 text-ot-text mt-1 border-ot-gray-700 border-b"
+                >
+                  {editingReviewId === item.id ? (
+                    <>
+                      <textarea
+                        placeholder="댓글을 입력하세요"
+                        value={editingReview}
+                        onChange={(e) => setEditingReview(e.target.value)}
+                        className="w-full text-ot-text placeholder-ot-gray-600 py-2 px-3 border border-ot-gray-800 rounded-lg"
+                      />
+                      <div className="flex items-center justify-end gap-3 mt-3">
+                        <button
+                          onClick={handleCancelEdit}
+                          className="text-sm px-3 py-1 hover:text-ot-gray-600 cursor-pointer"
+                        >
+                          취소
+                        </button>
+                        <CommonButton
+                          onClick={() => handleSaveEditReview(item.id)}
+                          className="px-2 py-1 rounded-sm"
+                        >
+                          <p className="text-sm">저장</p>
+                        </CommonButton>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <p className="flex-nowrap flex">{item.review}</p>
+
+                      <div className="flex items-center">
+                        <div className="flex text-sm pt-1">
+                          <p>
+                            {item.author} ⋅ {item.date}
+                          </p>
+                        </div>
+
+                        {item.myself && (
+                          <div className="flex justify-center items-center ml-auto">
+                            <button
+                              onClick={() =>
+                                handleEditReview(item.id, item.review)
+                              }
+                              className="text-sm flex mr-2 hover:text-ot-gray-600 cursor-pointer"
+                            >
+                              수정
+                            </button>
+                            <button
+                              onClick={() => handleDeleteReview(item.id)}
+                              className="text-sm flex hover:text-ot-gray-600 cursor-pointer"
+                            >
+                              삭제
+                            </button>
+                          </div>
+                        )}
+                      </div>
+                    </>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     );
@@ -270,66 +280,76 @@ export default function ReviewSection({
       </div>
 
       <div className="overflow-y-auto flex-1 mt-2">
-        {filteredSpoilerReviews.map((item) => (
-          <div
-            key={item.id}
-            className="flex flex-col p-3 text-ot-text mt-1 border-ot-gray-700 border-b"
-          >
-            {editingReviewId === item.id ? (
-              <>
-                <textarea
-                  placeholder="댓글을 입력하세요"
-                  value={editingReview}
-                  onChange={(e) => setEditingReview(e.target.value)}
-                  className="w-full text-ot-text placeholder-ot-gray-600 py-2 px-3 border border-ot-gray-800 rounded-lg"
-                />
-                <div className="flex items-center justify-end gap-3 mt-3">
-                  <button
-                    onClick={handleCancelEdit}
-                    className="text-sm px-3 py-1 hover:text-ot-gray-600 cursor-pointer"
-                  >
-                    취소
-                  </button>
-                  <CommonButton
-                    onClick={() => handleSaveEditReview(item.id)}
-                    className="px-2 py-1 rounded-sm"
-                  >
-                    <p className="text-sm">저장</p>
-                  </CommonButton>
-                </div>
-              </>
-            ) : (
-              <>
-                <p className="flex-nowrap flex">{item.review}</p>
-
-                <div className="flex items-center">
-                  <div className="flex text-sm pt-1">
-                    <p>
-                      {item.author} ⋅ {item.date}
-                    </p>
-                  </div>
-
-                  {item.myself && (
-                    <div className="flex justify-center items-center ml-auto">
-                      <button
-                        onClick={() => handleEditReview(item.id, item.review)}
-                        className="text-sm flex mr-2 hover:text-ot-gray-600 cursor-pointer"
-                      >
-                        수정
-                      </button>
-                      <button
-                        onClick={() => handleDeleteReview(item.id)}
-                        className="text-sm flex hover:text-ot-gray-600 cursor-pointer"
-                      >
-                        삭제
-                      </button>
-                    </div>
-                  )}
-                </div>
-              </>
-            )}
+        {filteredSpoilerReviews.length === 0 ? (
+          <div className="flex items-center justify-center h-full">
+            <p className="text-ot-gray-600">작성된 댓글이 없습니다.</p>
           </div>
-        ))}
+        ) : (
+          <div>
+            {filteredSpoilerReviews.map((item) => (
+              <div
+                key={item.id}
+                className="flex flex-col p-3 text-ot-text mt-1 border-ot-gray-700 border-b"
+              >
+                {editingReviewId === item.id ? (
+                  <>
+                    <textarea
+                      placeholder="댓글을 입력하세요"
+                      value={editingReview}
+                      onChange={(e) => setEditingReview(e.target.value)}
+                      className="w-full text-ot-text placeholder-ot-gray-600 py-2 px-3 border border-ot-gray-800 rounded-lg"
+                    />
+                    <div className="flex items-center justify-end gap-3 mt-3">
+                      <button
+                        onClick={handleCancelEdit}
+                        className="text-sm px-3 py-1 hover:text-ot-gray-600 cursor-pointer"
+                      >
+                        취소
+                      </button>
+                      <CommonButton
+                        onClick={() => handleSaveEditReview(item.id)}
+                        className="px-2 py-1 rounded-sm"
+                      >
+                        <p className="text-sm">저장</p>
+                      </CommonButton>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <p className="flex-nowrap flex">{item.review}</p>
+
+                    <div className="flex items-center">
+                      <div className="flex text-sm pt-1">
+                        <p>
+                          {item.author} ⋅ {item.date}
+                        </p>
+                      </div>
+
+                      {item.myself && (
+                        <div className="flex justify-center items-center ml-auto">
+                          <button
+                            onClick={() =>
+                              handleEditReview(item.id, item.review)
+                            }
+                            className="text-sm flex mr-2 hover:text-ot-gray-600 cursor-pointer"
+                          >
+                            수정
+                          </button>
+                          <button
+                            onClick={() => handleDeleteReview(item.id)}
+                            className="text-sm flex hover:text-ot-gray-600 cursor-pointer"
+                          >
+                            삭제
+                          </button>
+                        </div>
+                      )}
+                    </div>
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
