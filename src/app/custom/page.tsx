af88d3c30ab7062b003dda8fa3c5e901a2a27129
hyunw-kang <vpffp368@naver.com>
2026-03-02 23:08:01 +0900

    [OT-160] [FEAT]: 커스텀설정 UI 구현

--- src/app/custom/page.tsx
+++ src/app/custom/page.tsx
@@ -1,10 +1,12 @@
-import { Header, Footer } from "@/layouts";
+import { Header, Footer } from '@layouts';
+import { CustomSetting } from '@features/custom/components/CustomSetting';
 
-export default function CustomPage() { 
-    return (
-        <div>
-            <Header />
-            <Footer/>
-        </div>
-    )
+export default function CustomPage() {
+  return (
+    <div>
+      <Header />
+      <CustomSetting />
+      <Footer />
+    </div>
+  );
 }
