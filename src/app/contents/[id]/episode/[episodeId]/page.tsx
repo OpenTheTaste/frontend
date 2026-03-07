68c871e8f0f9b28d41a9b54495915b5629b1db51
joooii <3098k@naver.com>
2026-03-08 04:31:07 +0900

    [OT-215] [FIX]: 에피소드 페이지 임의 수정

--- src/app/contents/[id]/episode/[episodeId]/page.tsx
+++ src/app/contents/[id]/episode/[episodeId]/page.tsx
@@ -3,31 +3,28 @@ import { getContentById } from "@shared/mocks/mockContent";
 
 export default async function EpisodeDetailPage({
   params,
+  searchParams,
 }: {
-  params: Promise<{ id: string; episodeId: string }>;
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ type?: string }>;
 }) {
   const seriesId = Number((await params).id);
-  const episodeId = Number((await params).episodeId);
+  // const episodeId = Number((await params).episodeId);
+
+  const mediaId = Number((await params).id);
+  const mediaType = (await searchParams).type;
 
   const series = getContentById(seriesId);
 
-  if (!series || series.type !== "series") {
+  if (!series || series.mediaType !== "SERIES") {
     return <div>시리즈를 찾을 수 없습니다.</div>;
   }
 
-  const episode = series.episodes.find((ep) => ep.id === episodeId);
+  // const episode = series.episodes.find((ep) => ep.id === episodeId);
 
-  if (!episode) {
-    return <div>에피소드를 찾을 수 없습니다.</div>;
-  }
+  // if (!episode) {
+  //   return <div>에피소드를 찾을 수 없습니다.</div>;
+  // }
 
-  return (
-    <ContentsContainer
-      content={series}
-      isEpisodeView={true}
-      currentEpisodeId={episodeId}
-      seriesId={seriesId}
-      seriesTitle={series.title}
-    />
-  );
+  return <ContentsContainer mediaId={mediaId} mediaType={mediaType} />;
 }
