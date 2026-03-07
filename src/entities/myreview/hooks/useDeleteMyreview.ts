66839736c069f14756322284c1c9edb877918ab2
joooii <3098k@naver.com>
2026-03-08 04:28:48 +0900

    [OT-215] [FEAT]: 댓글 삭제 hooks queryKey 추가

--- src/entities/myreview/hooks/useDeleteMyreview.ts
+++ src/entities/myreview/hooks/useDeleteMyreview.ts
@@ -4,9 +4,11 @@ import { myreviewDeleteApi } from "@entities/myreview/api";
 export function useDeleteMyreview() {
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
-    mutationFn: (commentId: number) => myreviewDeleteApi.postMyreviewDelete(commentId),
+    mutationFn: (commentId: number) =>
+      myreviewDeleteApi.postMyreviewDelete(commentId),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["myreviews"] });
+      queryClient.invalidateQueries({ queryKey: ["review", "list"] });
     },
     onError: (err) => {
       console.error("댓글 삭제 실패:", err);
