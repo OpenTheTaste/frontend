d0bee49e2097683e38dfa4aeed2b58663e791195
RyuJaeHwi <yujaehwi02@gmail.com>
2026-03-09 17:31:54 +0900

    [OT-233][FIX]: 내가 쓴 댓글목록 타입 위치 변경

--- src/entities/myreview/api/myreviewApi.ts
+++ src/entities/myreview/api/myreviewApi.ts
@@ -1,5 +1,21 @@
 import { api } from "@shared/api";
-import { ApiResponse, MyReviewListResponse } from "@shared/types";
+import { ApiResponse, PageInfo } from "@shared/types";
+
+// 내가 쓴 댓글 형태 안쪽 타입
+export interface MyReview {
+  commentId: number;
+  content: string;
+  contentsPosterUrl: string;
+  writerId: number;
+  writerNickname: string;
+  createdDate: string;
+}
+
+// 내가 쓴 댓글 목록 조회될 때 타입
+export interface MyReviewListResponse {
+  pageInfo: PageInfo;
+  dataList: MyReview[];
+}
 
 export const myreviewApi = {
   getMyReviews: async (page: number) =>
