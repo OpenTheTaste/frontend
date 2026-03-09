cdf79c88e3a93560aab4b1f094d5e60232f660db
hyunw-kang <vpffp368@naver.com>
2026-03-09 17:33:33 +0900

    [OT-228] [FEAT]: User 시청이력 컴포넌트 구현

--- src/entities/home/components/index.ts
+++ src/entities/home/components/index.ts
@@ -3,3 +3,4 @@ export { default as ContentCarousel } from './ContentCarousel';
 export { default as TrendingCarousel } from './TrendingCarousel';
 export { default as RecommendCarousel } from "./RecommendCarousel";
 export { default as RecommendTagsCarousel } from "./RecommendTagsCarousel";
+export { default as HistoryCarousel } from "./HistoryCarousel";
