f36b358760ef91232d1e81c0a6fc1acd85beab82
joooii <3098k@naver.com>
2026-03-12 20:00:59 +0900

    [HOTFIX]: useHls cdn 추가

--- src/entities/player/hooks/useHls.ts
+++ src/entities/player/hooks/useHls.ts
@@ -4,47 +4,47 @@ import { useEffect, useRef } from "react";
 import Hls, { Level } from "hls.js";
 
 interface UseHlsProps {
-  src: string; // 영상 src
+  src: string;
   videoRef: React.RefObject<HTMLVideoElement | null>;
   onLevels?: (levels: Level[]) => void;
   startTime?: number;
 }
 
+const toProxySrc = (src: string) =>
+  src.replace("https://cdn.openthetaste.cloud", "/cdn-proxy");
+
 export const useHls = ({ src, videoRef, onLevels, startTime }: UseHlsProps) => {
   const hlsRef = useRef<Hls | null>(null);
 
   useEffect(() => {
-    // if (!videoRef.current) return;
     if (!videoRef.current || !src) return;
+
+    const proxySrc = toProxySrc(src);
 
     if (Hls.isSupported()) {
       const hls = new Hls();
       hlsRef.current = hls;
 
-      // FIXME: 아래 코드 주석 해제 해야 함
-      hls.loadSource(src); // m3u8 파일 로드
-
-      // hls.loadSource("https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"); // 임시테스트용
+      hls.loadSource(proxySrc);
       hls.attachMedia(videoRef.current);
-      // hls엔진을 video 태그에 연결 -> 여기서부터 video가 hls 기반으로 재생 가능하게 됨
 
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
         if (onLevels) {
-          onLevels(hls.levels); // 화질 목록 배열 생성 완료
+          onLevels(hls.levels);
         }
         if (startTime && videoRef.current) {
-          videoRef.current.currentTime = startTime; // 이어보기 구현
+          videoRef.current.currentTime = startTime;
         }
       });
 
       return () => {
         if (document.pictureInPictureElement) return;
-        // cleanup
         hls.destroy();
         hlsRef.current = null;
       };
     } else if (videoRef.current.canPlayType("application/vnd.apple.mpegurl")) {
-      videoRef.current.src = src; // safari 대응
+      videoRef.current.setAttribute("referrerpolicy", "no-referrer");
+      videoRef.current.src = proxySrc;
       if (startTime) videoRef.current.currentTime = startTime;
     }
   }, [src]);
