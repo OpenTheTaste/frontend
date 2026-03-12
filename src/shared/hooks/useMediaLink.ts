56f9b4992d049fb7868e3a53013416d4de226a73
RyuJaeHwi <yujaehwi02@gmail.com>
2026-03-12 21:45:01 +0900

    [OT-281][FEAT]: getMediaHref에 coomentId 타입 추가

--- src/shared/hooks/useMediaLink.ts
+++ src/shared/hooks/useMediaLink.ts
@@ -1,3 +1,5 @@
+// useMediaLink.ts
+
 import { MediaType, PlaylistSource } from "@shared/types";
 
 export const useMediaLink = () => {
@@ -5,35 +7,41 @@ export const useMediaLink = () => {
     mediaId: number,
     mediaType: MediaType,
     playlistSource?: PlaylistSource,
+    commentId?: number,
   ) => {
     const type = mediaType === "SERIES" ? "SERIES" : "CONTENTS";
     const base = `/contents/${mediaId}?type=${type}`;
 
-    if (!playlistSource || mediaType === "SERIES") return base;
-
     const params = new URLSearchParams();
-    switch (playlistSource.type) {
-      case "trending":
-        params.set("playlist", "trending");
-        break;
-      case "topTag":
-        params.set("playlist", "topTag");
-        params.set("index", String(playlistSource.index));
-        break;
-      case "search":
-        params.set("playlist", "search");
-        break;
-      case "recommend":
-        params.set("playlist", "recommend");
-        break;
-      case "history":
-        params.set("playlist", "history");
-        break;
-      case "bookmarks":
-        params.set("playlist", "bookmarks");
-        break;
+
+    if (playlistSource && mediaType !== "SERIES") {
+      switch (playlistSource.type) {
+        case "trending":
+          params.set("playlist", "trending");
+          break;
+        case "topTag":
+          params.set("playlist", "topTag");
+          params.set("index", String(playlistSource.index));
+          break;
+        case "search":
+          params.set("playlist", "search");
+          break;
+        case "recommend":
+          params.set("playlist", "recommend");
+          break;
+        case "history":
+          params.set("playlist", "history");
+          break;
+        case "bookmarks":
+          params.set("playlist", "bookmarks");
+          break;
+      }
     }
-    return `${base}&${params.toString()}`;
+
+    if (commentId) params.set("commentId", String(commentId));
+
+    const query = params.toString();
+    return query ? `${base}&${query}` : base;
   };
 
   return { getMediaHref };
