f14fb9db70a233d28c3d6bef538ec11e6bd44d6e
hyunw-kang <vpffp368@naver.com>
2026-03-12 14:58:35 +0900

    [OT-256] [FIX]: login 페이지에 약관 Link 추가

--- src/features/auth/components/login/LoginContent.tsx
+++ src/features/auth/components/login/LoginContent.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { useKakaoLogin } from "@/entities/auth/hooks";
 
 export default function LoginContent() {
   const { handleKakaoLogin } = useKakaoLogin();
 
   return (
-    <section className="w-full h-[70vh] bg-ot-background flex-1 flex items-center justify-center py-12">
+    <section className="w-full h-[73.5vh] bg-ot-background flex-1 flex items-center justify-center py-12">
       <div className="px-6 max-w-2xl mx-auto w-full flex flex-col items-center">
         <h1 className="text-[2rem] font-bold text-ot-text mb-4 text-center">
           로그인 정보를 입력해주세요
@@ -28,8 +29,9 @@ export default function LoginContent() {
           />
         </button>
         <p className="text-[1rem] text-ot-gray-600 text-center mt-6">
-          로그인 시 <span className="text-ot-text">이용약관</span> 및{" "}
-          <span className="text-ot-text">개인정보처리방침</span>에 동의하게
+          로그인 시 {" "}
+          <Link href="/policy/terms" className="text-ot-text hover:underline">이용약관</Link> 및{" "}
+          <Link href="/policy/privacy" className="text-ot-text hover:underline">개인정보처리방침</Link> 에 동의하게
           됩니다.
         </p>
       </div>
