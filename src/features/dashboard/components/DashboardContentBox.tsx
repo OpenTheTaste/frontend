8d711409351b9c3f08752eb7491c464973586bd4
joooii <3098k@naver.com>
2026-03-18 04:45:27 +0900

    [OT-347] [FIX]: 태그별 추천콘텐츠 Link 연결 + props drilling 최소화

--- src/features/dashboard/components/DashboardContentBox.tsx
+++ src/features/dashboard/components/DashboardContentBox.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { DashboardContentList } from "@features/dashboard/components";
-import { useTagRanking } from "@entities/dashboard/hooks/useTagRanking";
-import { DashboardData } from "@shared/types/mypage/dashboard";
+import { useTagRanking } from "@entities/dashboard/hooks";
+import { DashboardData } from "@shared/types/mypage";
 
 const COLORS = ["#5f001b", "#9c003e", "#ff5f7c", "#ffd1d8", "#f2f2f2"];
 
@@ -45,7 +45,6 @@ export default function DashboardContentBox() {
     tagIds: data?.rankings.map((tag) => tag.tagId) ?? [],
   };
 
-  // 테스트) 데이터 없을 때 모달창 띄울거면 아래 빈 거 체크하는 부분 주석으로 하기
   return (
     <div className="border-ot-text mx-auto flex w-full flex-col items-center rounded-lg border pt-6 pb-3">
       {data?.rankings.length === 0 ? (
--- src/features/dashboard/components/TagStatsModal.tsx
+++ src/features/dashboard/components/TagStatsModal.tsx
@@ -8,7 +8,6 @@ import {
   TagStatsModalList,
 } from "@features/dashboard/components";
 import { useOutsideClick } from "@shared/hooks";
-import { RecommendedContent } from "@shared/types/mypage/dashboard";
 
 interface TagStatsModalProps {
   isOpen: boolean;
@@ -20,7 +19,7 @@ interface TagStatsModalProps {
     thisMonth: number;
     lastMonth: number;
   };
-  recommendations: RecommendedContent[];
+  selectedTagId: number | null;
 }
 
 export default function TagStatsModal({
@@ -30,34 +29,26 @@ export default function TagStatsModal({
   isLoading,
   isError,
   monthlyStats,
-  recommendations,
+  selectedTagId,
 }: TagStatsModalProps) {
   const modalRef = useRef<HTMLDivElement>(null);
 
-  useOutsideClick(modalRef, onClose, isOpen); // 관련 hook 추가하여 사용
+  useOutsideClick(modalRef, onClose, isOpen);
 
   useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = "hidden"; // 모달창 열리면 뒷 원본 페이지 스크롤 기능 X
-      const handleEsc = (e: KeyboardEvent) => {
-        if (e.key === "Escape") {
-          onClose();
-        }
-      };
-      window.addEventListener("keydown", handleEsc); // ESC 누르면 모달창 닫음
-      return () => {
-        document.body.style.overflow = "unset";
-        window.removeEventListener("keydown", handleEsc);
-      };
-    }
+    if (!isOpen) return;
+    document.body.style.overflow = "hidden";
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => {
+      document.body.style.overflow = "unset";
+      window.removeEventListener("keydown", handleEsc);
+    };
   }, [isOpen, onClose]);
 
-  if (typeof window === "undefined") {
-    return null;
-  }
-  if (!isOpen) {
-    return null;
-  }
+  if (typeof window === "undefined" || !isOpen) return null;
 
   return createPortal(
     <div className="fixed inset-0 z-9999 flex items-center justify-center bg-black/50">
@@ -65,9 +56,8 @@ export default function TagStatsModal({
         ref={modalRef}
         className="bg-ot-gray-800 relative w-[60%] overflow-hidden rounded-xl pt-14 pb-8 shadow-2xl"
       >
-        {/* 닫기 버튼 */}
         <button
-          className="text-ot-text absolute top-7 right-7"
+          className="text-ot-gray-600 hover:text-ot-text absolute top-7 right-7 transition-all duration-150"
           onClick={onClose}
         >
           <X size={24} strokeWidth={2} />
@@ -83,14 +73,11 @@ export default function TagStatsModal({
           </div>
         ) : (
           <>
-            {/* 그래프 영역 */}
             <div className="px-25">
               <div className="relative flex flex-col items-center rounded-lg p-3">
                 <h2 className="text-ot-text text-[20px] font-bold">
                   {tagName} 시청 통계
                 </h2>
-
-                {/* 분리된 그래프 컴포넌트 호출 */}
                 <TagStatsModalGraph
                   tagName={tagName}
                   monthlyStats={monthlyStats}
@@ -98,17 +85,15 @@ export default function TagStatsModal({
               </div>
             </div>
 
-            {/* 구분선 */}
             <div className="px-15 py-2">
               <hr className="border-ot-gray-600" />
             </div>
 
-            {/* 태그별 추천 콘텐츠 영역 */}
             <div className="mt-2 px-15">
               <h3 className="text-ot-text text-[22px] font-bold">
                 태그별 추천 콘텐츠
               </h3>
-              <TagStatsModalList items={recommendations} />
+              <TagStatsModalList selectedTagId={selectedTagId} />
             </div>
           </>
         )}
