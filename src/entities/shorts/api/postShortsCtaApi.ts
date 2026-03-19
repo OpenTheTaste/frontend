83d9302c2724a78f143b20807cdede196c39a8ac
joooii <3098k@naver.com>
2026-03-19 09:34:21 +0900

    [OT-347] [FIX]: cta, events body mediaId로 변경

--- src/entities/shorts/api/postShortsCtaApi.ts
+++ src/entities/shorts/api/postShortsCtaApi.ts
@@ -1,6 +1,6 @@
 import { api } from "@shared/api";
 import { END_POINTS } from "@shared/constants";
 
-export const postShortsCtaApi = async (shortFormId: number) => {
-  await api.post(END_POINTS.SHORT_FORMS_CTA, { shortFormId });
+export const postShortsCtaApi = async (mediaId: number) => {
+  await api.post(END_POINTS.SHORT_FORMS_CTA, { mediaId });
 };
